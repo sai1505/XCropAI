3d81ce634456eea302422192e6182bea14477b31
sai1505 <vsai1505.bona@gmail.com>
2026-03-22 13:43:10 +0530

    Added vercel routing and changed frontend code.

--- client/src/components/Transitions/DashboardTransition.jsx
+++ client/src/components/Transitions/DashboardTransition.jsx
@@ -10,24 +10,12 @@ export default function DashboardTransition() {
 
     const ensureProfile = async () => {
         try {
+            const {
+                data: { session },
+            } = await supabase.auth.getSession();
 
-            let session = null;
 
-            // Wait until session is available (OAuth case)
-            for (let i = 0; i < 5; i++) {
-                const { data } = await supabase.auth.getSession();
-                session = data.session;
-
-                if (session) break;
-
-                await new Promise((res) => setTimeout(res, 300));
-            }
-
-            if (!session) {
-                console.warn("No session found");
-                navigate("/login");
-                return;
-            }
+            if (!session) return;
 
             const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/users/verify-profile`, {
                 method: "POST",
@@ -44,9 +32,9 @@ export default function DashboardTransition() {
                 return;
             }
 
-            if (!res.ok) return false;
-
-            return true;
+            if (!res.ok) {
+                throw new Error("Profile verification failed");
+            }
 
         } catch (err) {
             console.error(err);
@@ -58,14 +46,15 @@ export default function DashboardTransition() {
         if (hasInitialized.current) return;
         hasInitialized.current = true;
 
-        const init = async () => {
-            const success = await ensureProfile();
 
-            if (success) {
+        const init = async () => {
+
+            //Profile Check
+            await ensureProfile();
+
+            setTimeout(() => {
                 navigate("/dashboard/new", { replace: true });
-            } else {
-                navigate("/login");
-            }
+            }, 3000);
         };
 
         init();
